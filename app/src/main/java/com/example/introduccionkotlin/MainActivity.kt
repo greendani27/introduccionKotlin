fafe2cca808daaa81b082d603d6a4eea22b1f292
greendani27 <94862098+greendani27@users.noreply.github.com>
2022-07-30 03:27:12 +0200

    Innit commit

--- app/src/main/java/com/example/introduccionkotlin/MainActivity.kt
+++ app/src/main/java/com/example/introduccionkotlin/MainActivity.kt
@@ -7,17 +7,24 @@ class MainActivity : AppCompatActivity() {
     companion object {
         const val moneda = "EUR"
     }
+
+    var saldo: Float = 300.54f
+    var sueldo = 764.82f
+    var entero: Int = 62
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
         val fecha = "25/06/2002"
-        var nombre = "dani"
+        var nombre: String = "dani"
+        var vip: Boolean = false
+        var inicial: Char = 'J'
 
-        nombre = "Dani"
-        //fecha = "25/06/2003"
-        println(nombre)
+        var saludo = "hola $nombre"
+
+        println(saludo)
 
     }
 }
